cc2b7d0eb77fcdfd55390a9c6d44ff482a4befd1
Julian Doherty <madlep@madlep.com>
2022-09-30 20:24:53 +1000

    Update dot_config/nvim/lua/user/autocmds.lua Update dot_config/nvim/lua/user/lsp/init.lua

--- dot_config/nvim/lua/user/autocmds.lua
+++ dot_config/nvim/lua/user/autocmds.lua
@@ -1,2 +1,5 @@
 -- make uses tabs
 vim.cmd [[ autocmd FileType make set noexpandtab shiftwidth=8 softtabstop=0 ]]
+
+-- auto format on save
+vim.cmd [[autocmd BufWritePre *.ex,*.exs,*.rb,*.lua lua vim.lsp.buf.formatting_sync()]]
--- dot_config/nvim/lua/user/lsp/init.lua
+++ dot_config/nvim/lua/user/lsp/init.lua
@@ -2,6 +2,3 @@ require("user.lsp.ui")
 require("user.lsp.lspconfig")
 require("user.lsp.null-ls")
 require("user.lsp.codelens").setup()
-
--- auto format on save
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
