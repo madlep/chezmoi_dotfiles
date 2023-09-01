bd63ac95dba9e369cade09c4c4f5cbf20303a703
Julian Doherty <jdoherty@wesdigital.com.au>
2023-09-01 12:54:30 +1000

    Update .config/nvim/lua/user/lsp/null-ls/prettier.lua

--- dot_config/nvim/lua/user/lsp/null-ls/prettier.lua
+++ dot_config/nvim/lua/user/lsp/null-ls/prettier.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
     return {
-        extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+        -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     }
 end
 
