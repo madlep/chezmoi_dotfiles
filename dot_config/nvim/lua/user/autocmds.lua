4732da4736779882b7a0dd5d06749f5f296eac64
Julian Doherty <jdoherty@wesdigital.com.au>
2024-06-12 11:14:15 +1000

    Add .config/nvim/after/ftplugin/go.lua Add .config/nvim/after/ftplugin/lua.lua Add .config/nvim/after/ftplugin/make.lua Add .config/nvim/after/ftplugin/qf.lua Update .config/nvim/lua/user/autocmds.lua

--- dot_config/nvim/lua/user/autocmds.lua
+++ dot_config/nvim/lua/user/autocmds.lua
@@ -1,13 +1,3 @@
--- make uses tabs
-vim.cmd([[ autocmd FileType make set noexpandtab shiftwidth=4 softtabstop=0 ]])
--- lua has 4 space indentation - lua ls needs this set
-vim.cmd([[ autocmd FileType lua set shiftwidth=4 expandtab tabstop=4 softtabstop=4]])
-
--- go uses tabs
-vim.cmd([[ autocmd FileType go set noexpandtab tabstop=4 shiftwidth=4 softtabstop=0 ]])
-
-vim.cmd([[ autocmd FileType qf set nobuflisted ]])
-
 -- auto format on save
 vim.cmd([[ autocmd BufWritePre *.ex,*.exs,*.rb,*.lua,*.rs,*.go,*.ts lua vim.lsp.buf.format()]])
 --
